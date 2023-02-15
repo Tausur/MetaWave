0fb3f28f31565b9b3a1fa0e3247ca6ba28df2434
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2023-02-15 19:32:32 +0600

    Add files via upload

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Typical from 'react-typical'
+import Typewriter from 'typewriter-effect'
 
 const HomePage = (props: { theme: any }) => {
   return (
@@ -8,13 +8,21 @@ const HomePage = (props: { theme: any }) => {
         <div className="profile bg-blue-500 rounded-full flex justify-center items-center mx-5 mb-3">
           <img src="/profile.jpg" alt="" className='rounded-full w-48 h-48 border-2 border-white pointer-events-none' />
         </div>
-        <div className="typewriter">
+        <div className="typewriter text-white">
           <p className='text-2xl'>{"Hi, I'm Tausur Rahaman"}</p>
-            <Typical
-              steps={["I'm a developer", 2000, "I'm a designer", 2000, "I'm a scientist", 2000]}
-              loop={Infinity}
-              wrapper="b"
+          <h1>
+            <Typewriter
+            options={{
+              strings : [
+                "Web developer",
+                "Game developer",
+                "UI/UX developer"
+              ],
+              autoStart : true,
+              loop : true
+            }}
             />
+          </h1>
         </div>
       </div>
 
